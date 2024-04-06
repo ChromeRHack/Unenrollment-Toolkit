58f63c085e851521684f769f1d6b99773c7a8a9f
Peap <Lviigurarie@gmail.com>
2024-04-05 20:55:36 -0600

    Updated UTK logo in mush

--- mush.sh
+++ mush.sh
@@ -24,7 +24,7 @@ traps() {
 }
 
 mush_info() {
-    echo -ne "\033]0;mush\007"
+    echo -ne "\033]0;UTK\007"
     if [ ! -f /mnt/stateful_partition/custom_greeting ]; then
         cat <<-EOF
 Welcome to mush, the UTK developer shell.
