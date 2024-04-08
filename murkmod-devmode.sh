4e6bd7beb5f1ade1ae691767901b0371f502432d
Peap <123750838+PeapGit@users.noreply.github.com>
2024-04-08 12:10:31 -0600

    Update murkmod-devmode.sh

--- murkmod-devmode.sh
+++ murkmod-devmode.sh
@@ -245,10 +245,11 @@ EOF
         cgpt add "$dst" -i 4 -P 0
         cgpt add "$dst" -i 2 -P 0
         cgpt add "$dst" -i "$tgt_kern" -P 1
+        output = 0
         if [[ ${#output} -eq 1 ]]; then
             echo "Defogging... This will set GBB flags to 0x8091"
-            fi
             defog
+            fi
         echo "Cleaning up..."
         losetup -d "$loop"
         rm -f "$FILENAME"
