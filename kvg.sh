b6eae3dc603c67a7d824fee5df9dc13b30ed1161
Peap <Lviigurarie@gmail.com>
2024-04-27 17:53:08 -0600

    not writing the whole thing neither how this works just in case we publish before kvs v2

--- kvg.sh
+++ kvg.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-# Input from user
-read -p "Enter a string: " input
+echo "KVG: KERNEL VERSION GENERATOR"
+echo "MADE BY PEAP"
+echo "ORIGINALLY MADE BY KXTZOWNSU"
+echo "LICENSED UNDER THE AGPLv3"
+echo "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="
+read -p "Enter the kernel version you would like to use: " input
+echo "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-"
 
-# Remove "0x" prefix
+    # Remove "0x" prefix
 input=${input#"0x"}
 
-# Reverse the string
+    # Reverse the string
 reversed=$(echo "$input" | rev)
 
-# Insert spaces every two characters
+    # Insert spaces every two characters
 spaced=$(echo "$reversed" | sed 's/../& /g')
 
-# Print the final result
-echo "Modified string: $spaced"
-
-main() {
-    local output="02 4c 57 52 47 $spaced 00 00 00 eb"
-    echo $output > kernver
-}
-
-main
+hex_input="02 4c 57 52 47 aa aa aa 00 00 00 00 74"
+echo "$hex_input" > kernver
+echo "Here is your kernver in hex:"
+echo "$hex_input"
