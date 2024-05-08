43548cd30faffd1bb8cf125050e0e1dfdb0cc293
Codeguy <148151420+codeguy271@users.noreply.github.com>
2024-05-08 07:53:16 -0600

    Update murkmod-devmode.sh

--- murkmod-devmode.sh
+++ murkmod-devmode.sh
@@ -211,16 +211,6 @@ murkmod() {
     else
         show_logo_recovery
     fi
-    echo "No match found on chrome100. Falling back to Chromium Dash. Because we are debug and chrome100.dev is down this is temp. Versions only go down to 114."
-    local builds=$(curl -ks "https://chromiumdash.appspot.com/cros/fetch_serving_builds?deviceCategory=Chrome%20OS\\")
-    local hwid=$(jq "(.builds.$board[] | keys)[0]" <<<"$builds")
-    local hwid=${hwid:1:-1}
-
-    # Get all milestones for the specified hwid
-    milestones=$(jq ".builds.$board[].$hwid.pushRecoveries | keys | .[]" <<<"$builds")
-
-    # Loop through all milestones
-    echo "Would you l"
     echo "Finding latest Chrome100 build ID..."
     local build_id=$(curl -s "https://chrome100.dev" | grep -o '"buildId":"[^"]*"' | cut -d':' -f2 | tr -d '"')
     echo "Finding recovery image..."
@@ -240,15 +230,43 @@ murkmod() {
     chrome_versions=$(echo "$json" | jq -r '.pageProps.images[].chrome')
     echo "Found $(echo "$chrome_versions" | wc -l) versions of chromeOS for your board on chrome100."
     echo "Searching for a match..."
-    for milestone in $milestones; do
-        milestone=$(echo "$milestone" | tr -d '"')
-        if [[ $milestone == $VERSION* ]]; then
+    MATCH_FOUND=0
+    for cros_version in $chrome_versions; do
+        platform=$(echo "$json" | jq -r --arg version "$cros_version" '.pageProps.images[] | select(.chrome == $version) | .platform')
+        channel=$(echo "$json" | jq -r --arg version "$cros_version" '.pageProps.images[] | select(.chrome == $version) | .channel')
+        mp_token=$(echo "$json" | jq -r --arg version "$cros_version" '.pageProps.images[] | select(.chrome == $version) | .mp_token')
+        mp_key=$(echo "$json" | jq -r --arg version "$cros_version" '.pageProps.images[] | select(.chrome == $version) | .mp_key')
+        last_modified=$(echo "$json" | jq -r --arg version "$cros_version" '.pageProps.images[] | select(.chrome == $version) | .last_modified')
+        # if $cros_version starts with $VERSION, then we have a match
+        if [[ $cros_version == $VERSION* ]]; then
+            echo "Found a $VERSION match on platform $platform from $last_modified."
             MATCH_FOUND=1
-            FINAL_URL=$(jq -r ".builds.$board[].$hwid.pushRecoveries[\"$milestone\"]" <<<"$builds")
-            echo "Found a match!"
+            #https://dl.google.com/dl/edgedl/chromeos/recovery/chromeos_15117.112.0_hatch_recovery_stable-channel_mp-v6.bin.zip
+            FINAL_URL="https://dl.google.com/dl/edgedl/chromeos/recovery/chromeos_${platform}_${board}_recovery_${channel}_${mp_token}-v${mp_key}.bin.zip"
             break
         fi
     done
+    if [ $MATCH_FOUND -eq 0 ]; then
+        echo "No match found on chrome100. Falling back to Chromium Dash."
+        local builds=$(curl -ks "https://chromiumdash.appspot.com/cros/fetch_serving_builds?deviceCategory=Chrome%20OS\\")
+        local hwid=$(jq "(.builds.$board[] | keys)[0]" <<<"$builds")
+        local hwid=${hwid:1:-1}
+
+        # Get all milestones for the specified hwid
+        milestones=$(jq ".builds.$board[].$hwid.pushRecoveries | keys | .[]" <<<"$builds")
+
+        # Loop through all milestones
+        echo "Searching for a match..."
+        for milestone in $milestones; do
+            milestone=$(echo "$milestone" | tr -d '"')
+            if [[ $milestone == $VERSION* ]]; then
+                MATCH_FOUND=1
+                FINAL_URL=$(jq -r ".builds.$board[].$hwid.pushRecoveries[\"$milestone\"]" <<<"$builds")
+                echo "Found a match!"
+                break
+            fi
+        done
+    fi
 
     if [ $MATCH_FOUND -eq 0 ]; then
         echo "No recovery image found for your board and target version. Exiting."
