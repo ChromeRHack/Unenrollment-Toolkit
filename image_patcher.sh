d010250c78df5c2a9ecc65693ad9317976028715
kmzheng10 <167134143+kmzheng10@users.noreply.github.com>
2024-05-08 08:41:51 -0600

    Update image_patcher.sh

--- image_patcher.sh
+++ image_patcher.sh
@@ -39,11 +39,11 @@ ascii_info() {
     # spaces get mangled by makefile, so this must be separate
 }
 nullify_bin() {
-    cat <<-EOF >$1
+    cat <<-EOF >"$1"
 #!/bin/bash
 exit
 EOF
-    chmod 777 $1
+    chmod 777 "$1"
     # shebangs crash makefile
 }
 
@@ -193,7 +193,7 @@ main() {
   traps
   ascii_info
   configure_binaries
-  echo $SSD_UTIL
+  echo "$SSD_UTIL"
 
   if [[ -z $1 ]] || [[ ! -f $1 ]]; then
     echo "\"$1\" isn't a real file, dipshit! You need to pass the path to the recovery image. Optional args: <path to custom bootsplash: path to a png> <unfuck stateful: int 0 or 1>"
@@ -202,7 +202,7 @@ main() {
   if [ -z "$2" ]; then
     echo "Not using a custom bootsplash."
     local bootsplash="0"
-  elif [ ! -f $2 ]; then
+  elif [ ! -f "$2" ]; then
     echo "file $2 not found for custom bootsplash"
     local bootsplash="0"
   else
@@ -226,9 +226,9 @@ main() {
   losetup -P "$loop" "$bin"
 
   echo "Disabling kernel verity..."
-  $SSD_UTIL --debug --remove_rootfs_verification -i ${loop} --partitions 4
+  $SSD_UTIL --debug --remove_rootfs_verification -i "${loop}" --partitions 4
   echo "Enabling RW mount..."
-  $SSD_UTIL --debug --remove_rootfs_verification --no_resign_kernel -i ${loop} --partitions 2
+  $SSD_UTIL --debug --remove_rootfs_verification --no_resign_kernel -i "${loop}" --partitions 2
 
   # for good measure
   sync
@@ -243,14 +243,14 @@ main() {
   if [ "$bootsplash" != "0" ]; then
     echo "Adding custom bootsplash..."
     for i in $(seq -f "%02g" 0 30); do
-      rm $ROOT/usr/share/chromeos-assets/images_100_percent/boot_splash_frame${i}.png
+      rm $ROOT/usr/share/chromeos-assets/images_100_percent/boot_splash_frame"${i}".png
     done
-    cp $bootsplash $ROOT/usr/share/chromeos-assets/images_100_percent/boot_splash_frame00.png
+    cp "$bootsplash" $ROOT/usr/share/chromeos-assets/images_100_percent/boot_splash_frame00.png
   else
     echo "Adding murkmod bootsplash..."
     install "chromeos-bootsplash-v2.png" /tmp/bootsplash.png
     for i in $(seq -f "%02g" 0 30); do
-      rm $ROOT/usr/share/chromeos-assets/images_100_percent/boot_splash_frame${i}.png
+      rm $ROOT/usr/share/chromeos-assets/images_100_percent/boot_splash_frame"${i}".png
     done
     cp /tmp/bootsplash.png $ROOT/usr/share/chromeos-assets/images_100_percent/boot_splash_frame00.png
     rm /tmp/bootsplash.png
